eb085683b7d677816fa05336fd5c6b698f21ebba
thenhhduu <dung26762@gmail.com>
2021-04-15 14:44:00 +0700

    nocommit

---
+++ includes/header.php
@@ -0,0 +1,61 @@
+<header>
+	
+	<div class="nav_toggle" onclick="toggle_class();">
+		
+		<span class="toggle_icon"></span>
+		
+	</div>
+	
+	<div onclick="remove_class()">
+	
+	<div class="main_nav">
+		
+		<h2>Unique Restaurant</h2>
+		
+		<ul class="default_links">
+			
+			<li><a href="index.php">Trang Chủ</a></li>
+			<li><a href="menu.php">Menu</a></li>
+			<li><a href="reservation.php">Đặt Bàn</a></li>
+			<li><a href="gallery.php">Gallery</a></li>
+			<li><a href="basket.php">Giỏ Hàng</a></li>
+			
+		</ul>
+		
+		<p class="clear"></p>
+		
+	</div>
+	
+	<p class="clear"></p>
+	
+	</div>
+	
+</header>
+
+<div class="responive_nav">
+	
+	<div class="nav_section_img">
+		
+		<div class="nav_section_div">
+			
+			<h3>Unique Restaurant</h3>
+			
+		</div>
+		
+	</div>
+	
+	<div class="nav_section">
+		
+		<ul>
+			
+			<li><a href="index.php"><span class="home">Trang Chủ</span></a></li>
+			<li><a href="menu.php"><span class="menu">Menu</span></a></li>
+			<li><a href="reservation.php"><span class="reserve">Đặt Bàn</span></a></li>
+			<li><a href="gallery.php"><span class="gallery">Gallery</span></a></li>
+			<li><a href="basket.php"><span class="order">Order</span></a></li>
+			
+		</ul>
+		
+	</div>
+	
+</div>
